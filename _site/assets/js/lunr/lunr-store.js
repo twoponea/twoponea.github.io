83a3512384b385a9d541f48b32bcbdb1fcc22028
twoponea <twoponea@gmail.com>
2022-09-02 01:48:05 +0700

    update

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,6 +2,6 @@ var store = [{
         "title": "First Post",
         "excerpt":"This is the first post for testing.  ","categories": ["General"],
         "tags": ["General"],
-        "url": "/general/2022/09/02/very-first-post.html",
+        "url": "/general/2022/09/01/very-first-post.html",
         "teaser": null
       }]
